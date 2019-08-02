02f59691d6a1abfdcde53656ac13456719b45b52
Ilyas979 <41782122+Ilyas979@users.noreply.github.com>
2019-08-02 17:49:26 +0200

    Add files via upload

--- Code/create_hists_ttH.py
+++ Code/create_hists_ttH.py
@@ -17,13 +17,24 @@ ttbar_df = ttbar.pandas.df().drop(columns = unwanted_features)
 
 feature_names = ttbar_df.columns
 
-for inst, label, src_name in zip(data_insts, data_labels, data_name):
-  for feat_name, i in zip(feature_names, range(len(feature_names))):
+for feat_name, i in zip(feature_names, range(len(feature_names))):
+  for inst, label, src_name in zip(data_insts, data_labels, data_name):
     a = inst.toarray()[:,i][label.reshape(-1) == 1]
     b = inst.toarray()[:,i][label.reshape(-1) == 0]
-    bins = np.histogram(np.hstack((a,b)), bins=40)[1]
-    plt.hist(a, bins, alpha=0.5, label='sgnl', density = True)
-    plt.hist(b, bins, alpha=0.5, label='bkg', density = True)
+    if src_name == 'src':
+      bins = np.histogram(np.hstack((a,b)), bins=40)[1]
+      plt.hist(a, bins, alpha=0.5, label='sgnl', density = True)
+      plt.hist(b, bins, alpha=0.5, label='bkg', density = True)
+      x_lim = plt.xlim()
+      y_lim = plt.ylim()
+      l = list(y_lim)
+      l[1] *= 1.1
+      y_lim = tuple(l)
+    else:
+      plt.hist(a, bins, alpha=0.5, label='sgnl', density = True)
+      plt.hist(b, bins, alpha=0.5, label='bkg', density = True)
+    plt.xlim(x_lim)
+    plt.ylim(y_lim)
     plt.legend(loc='upper right')
     plt.title(feat_name + ", " + src_name)
     plt.savefig("../Plots/Histograms/"+feat_name+ "_" + src_name)
