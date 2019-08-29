8122b75b77af9dc1b4e69b01f54b434d37c9438d
Ilyas979 <41782122+Ilyas979@users.noreply.github.com>
2019-08-29 02:21:15 +0200

    Add files via upload

--- Code/root_to_pkl.py
+++ Code/root_to_pkl.py
@@ -6,26 +6,23 @@ import pickle
 from scipy.sparse import coo_matrix
 from sklearn.preprocessing import StandardScaler
 
-unwanted_features = ['weight_mc', 'weight_xsec', 'weight_total', 'OverlapRemove', 'Mbbbb_Max']
-#highly_correlated_features = ['Mbj_MaxPt', 'HT_jets']
-#unwanted_features.extend(highly_correlated_features)
-lucky_bad_features = ['Aplanarity_bjets', 'dRlb2', 'dRlj_MaxdR']
-unwanted_features.extend(lucky_bad_features)
+unwanted_features = ['weight_mc', 'weight_xsec', 'weight_total', 'OverlapRemove']
+highly_correlated_features = ['Mbbbb_Max', 'HT_all']
+unwanted_features.extend(highly_correlated_features)
 
 ttH = uproot.open("../Data/pre_selected_tev13_mg5_ttH_0001.root")["nominalAfterCuts"]
 ttH_df = ttH.pandas.df().drop(columns = unwanted_features)
-#ttH_df = ttH_df.loc[:, ~ttH_df.columns.duplicated()]
+ttH_df = ttH_df.loc[:, ~ttH_df.columns.duplicated()]
 ttH_np = ttH_df.values
-print(ttH_df.columns)
 
 ttbar = uproot.open("../Data/pre_selected_tev13_mg5_ttbar_0001.root")["nominalAfterCuts"]
 ttbar_df = ttbar.pandas.df().drop(columns = unwanted_features)
-#ttbar_df = ttbar_df.loc[:, ~ttbar_df.columns.duplicated()]
+ttbar_df = ttbar_df.loc[:, ~ttbar_df.columns.duplicated()]
 ttbar_np = ttbar_df.values
 
 ttbb = uproot.open("../Data/pre_selected_tev13_ttbb_PP8delphes.root")["nominalAfterCuts"]
 ttbb_df = ttbb.pandas.df().drop(columns = ['scalePDF'] + unwanted_features)
-#ttbb_df = ttbb_df.loc[:, ~ttbb_df.columns.duplicated()]
+ttbb_df = ttbb_df.loc[:, ~ttbb_df.columns.duplicated()]
 ttbb_np = ttbb_df.values
 
 assert((ttH_df.columns == ttbar_df.columns).all())
@@ -33,7 +30,6 @@ assert((ttH_df.columns == ttbb_df.columns).all())
 
 print(ttH_np.shape, ttbar_np.shape, ttbb_np.shape)
 
-#maybe suffle first
 np.random.shuffle(ttH_np)
 np.random.shuffle(ttbar_np)
 np.random.shuffle(ttbb_np)
@@ -48,8 +44,6 @@ y_src = np.concatenate((np.ones(N_ttH_events),np.zeros(N_bkg_events)))
 
 target = coo_matrix(scaler.transform(np.concatenate((ttH_np[N_ttH_events:2*N_ttH_events], ttbb_np[:N_bkg_events])))).tocsc()
 y_target = np.concatenate((np.ones(N_ttH_events),np.zeros(N_bkg_events)))
-#target = coo_matrix(StandardScaler().fit_transform(np.concatenate((ttbb_np[N_ttH_events:2*N_ttH_events], ttbb_np[:N_bkg_events])))).tocsc()
-#y_target = np.concatenate((np.zeros(N_ttH_events),np.zeros(N_bkg_events)))
 
 
 data_insts, data_labels, num_insts = [], [], []
@@ -76,11 +70,24 @@ with open('../Data/data_src_vs_trg', 'wb') as f:
     pickle.dump(data_name, f)
 
 print("Data is saved to 'data_src_vs_trg' in pickle format")
-
 ############
 
-src1 = coo_matrix(StandardScaler().fit_transform(np.concatenate((ttH_np[N_ttH_events:2*N_ttH_events], ttbar_np[N_bkg_events:2*N_bkg_events])))).tocsc()
+with open('../Data/data_trg_vs_src', 'wb') as f:
+    pickle.dump(list(reversed(data_insts)), f)
+    pickle.dump(list(reversed(data_labels)), f)
+    pickle.dump(list(reversed(data_name)), f)
+
+print("Data is saved to 'data_trg_vs_src' in pickle format")
+############
+
+scaler = StandardScaler()
+scaler.fit(np.concatenate((ttH_np[:2*N_ttH_events], ttbar_np[:2*N_bkg_events])))
+
+src1 = coo_matrix(scaler.transform(np.concatenate((ttH_np[N_ttH_events:2*N_ttH_events], ttbar_np[N_bkg_events:2*N_bkg_events])))).tocsc()
 y_src1 = np.concatenate((np.ones(N_ttH_events),np.zeros(N_bkg_events)))
+
+#src1 = coo_matrix(StandardScaler().fit_transform(np.concatenate((ttH_np[N_ttH_events:2*N_ttH_events], ttbar_np[N_bkg_events:2*N_bkg_events])))).tocsc()
+#y_src1 = np.concatenate((np.ones(N_ttH_events),np.zeros(N_bkg_events)))
 
 data_insts, data_labels, num_insts = [], [], []
 data_name = ['src', 'src1']
@@ -107,7 +114,7 @@ with open('../Data/data_src_vs_src1', 'wb') as f:
     pickle.dump(data_name, f)
 
 print("Data is saved to 'data_src_vs_src1' in pickle format")
-'''
+
 ############
 target1 = coo_matrix(StandardScaler().fit_transform(np.concatenate((ttH_np[:N_ttH_events], ttbb_np[N_bkg_events:2*N_bkg_events])))).tocsc()
 y_target1 = np.concatenate((np.ones(N_ttH_events),np.zeros(N_bkg_events)))
@@ -137,4 +144,4 @@ with open('../Data/data_trg_vs_trg1', 'wb') as f:
     pickle.dump(data_name, f)
 
 print("Data is saved to 'data_trg_vs_trg1' in pickle format")
-'''
+
